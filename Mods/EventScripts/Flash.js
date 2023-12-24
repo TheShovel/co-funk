f7f152dce28955c044804cf15e51870c7d555f57
TheShovel <niccata24@gmail.com>
2023-12-24 18:45:30 +0200

    1.5

--- Mods/EventScripts/Flash.js
+++ Mods/EventScripts/Flash.js
@@ -1,4 +1,4 @@
-const flashTime = Number(getLevelVariable("value1"))*250;
+const flashTime = Number(getLevelVariable("value1"))*100;
 const delay = ms => new Promise(res => setTimeout(res, ms));
 vm.runtime.ext_theshovelcanvaseffects.changeEffect({"EFFECT":"brightness","NUMBER":100});
 const flash = async () => {
---
+++ Mods/EventScripts/shaderApply.js
@@ -0,0 +1,5 @@
+var effect = '';
+vm.runtime.ext_SPspriteEffects.setSpriteEffect({"EFFECT":getLevelVariable("value1"),"NUM":getLevelVariable("value2")}).then(result =>
+{effect = result; 
+    vm.runtime.ext_SPspriteEffects.addCanvasFilter({"FILTER":effect,"NAME":Math.random()+''})
+});
