416ad114a22af3fb19d00f0e1eed662bbc8b6da0
TheShovel <niccata24@gmail.com>
2024-01-21 19:43:15 +0200

    disable shaders option

--- Mods/EventScripts/shaderApply.js
+++ Mods/EventScripts/shaderApply.js
@@ -1,5 +1,6 @@
+if (getLevelVariable("disableShaders") != "true"){
 var effect = '';
 vm.runtime.ext_SPspriteEffects.setSpriteEffect({"EFFECT":getLevelVariable("value1"),"NUM":getLevelVariable("value2")}).then(result =>
 {effect = result; 
     vm.runtime.ext_SPspriteEffects.addCanvasFilter({"FILTER":effect,"NAME":Math.random()+''})
-});
+});};
